44bbd9ed2b7b1c1e262463382f2bde481c65f0e4
Aragajorge <3glasser@gmail.com>
2024-05-12 16:35:40 -0300

    Colocando a leitura do números dentro do bloco try-catch

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-
-        System.out.println("Digite o primeiro parâmetro: ");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro: ");
-        int parametroDois = terminal.nextInt();
 
         try {
+            Scanner terminal = new Scanner(System.in);
+
+            System.out.println("Digite o primeiro parâmetro: ");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro: ");
+            int parametroDois = terminal.nextInt();
             // chamndo o método contendo a logica de contagem
             contar(parametroUm, parametroDois);
 
         } catch (InputMismatchException e) {
 
-            System.err.println("Você só pode digitar números!");
+            System.out.println("Você só pode digitar números!");
 
         } catch (ParametrosInvalidosException e) {
             // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
